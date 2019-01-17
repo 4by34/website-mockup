3982d75671a0da1c5b45ee07f8f5518881ebf1cb
4by34 <fourbythirtyfour@gmail.com>
2019-01-16 23:25:16 -0500

    fixed the sticky navbar and added links to images

--- js/index.js
+++ js/index.js
@@ -1,32 +1,30 @@
+window.onscroll = function() {fixedNav()};
+
+var navOffset = jQuery(".activateStick").offset().top;
+
+function fixedNav() {
+	if (window.pageYOffset > navOffset) {
+		jQuery(".activateStick").addClass("fixed-top");
+		jQuery(".pageHeader").addClass("stickyBody");
+		jQuery(".navbar-brand").addClass("showLogo");
+		jQuery(".navbar-brand").removeClass("hideLogo");
+		jQuery(".mainBody").css("margin-top", "72px");
+	} else {
+		jQuery(".activateStick").removeClass("fixed-top");
+		jQuery(".pageHeader").removeClass("stickyBody");
+		jQuery(".navbar-brand").addClass("hideLogo");
+		jQuery(".navbar-brand").removeClass("showLogo");
+		jQuery(".mainBody").css("margin-top", "0");
+	}
+}
+
 jQuery(document).ready(function() {
-
-	var navOffset = jQuery(".activateStick").offset().top;
-
-	jQuery(window).scroll(function() {
-		var scrollPos = jQuery(window).scrollTop();
-
-		jQuery(".navbar-collapse").removeClass("in");
-
-		if (scrollPos >= navOffset) {
-		  jQuery(".activateStick").addClass("fixed-top");
-		  jQuery(".pageHeader").addClass("stickyBody");
-		  jQuery(".navbar-brand").addClass("showLogo");
-		  jQuery(".navbar-brand").removeClass("hideLogo");
-		  jQuery(".mainBody").css("margin-top", "72px");
-		} else {
-		  jQuery(".activateStick").removeClass("fixed-top");
-		  jQuery(".pageHeader").removeClass("stickyBody");
-		  jQuery(".navbar-brand").addClass("hideLogo");
-		  jQuery(".navbar-brand").removeClass("showLogo");
-		  jQuery(".mainBody").css("margin-top", "0");
-		}
-
-	});
 
 	function show_banner() {
 		jQuery("#banner").removeClass("hideLogo");
 		jQuery("#banner").addClass("showLogoSlow");
 	};
 
-	window.setTimeout( show_banner, 800 );
+	window.setTimeout( show_banner, 900 );
 });
+
